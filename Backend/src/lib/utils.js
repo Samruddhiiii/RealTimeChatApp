ed7ba0de4718e9ba1167d4d03083eb826d551bca
Samruddhi_Gulhane <90473980+Samruddhiiii@users.noreply.github.com>
2025-08-04 20:04:54 +0530

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken= (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
-        expiresIn:"7d",
-    });
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 1000, 
-        httpOnly: true,  // prevents an attack on cross site scrpting attacks
-        sameSite: "strict", 
-        secure: process.env.NODE_ENV !== "development", 
-    });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-    return  token;
+  return token;
 };
