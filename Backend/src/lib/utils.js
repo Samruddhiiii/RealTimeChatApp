a74207a7557f8612de1a6933a27b7bb654e57b09
Samruddhi_Gulhane <90473980+Samruddhiiii@users.noreply.github.com>
2025-08-04 23:00:53 +0530

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -6,11 +6,12 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    httpsOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // ✅ prevents JS access
+    secure: process.env.NODE_ENV !== "development", // ✅ ensures cookie only on HTTPS in prod
+    sameSite: "none", // ✅ allows cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
   return token;
 };
+
