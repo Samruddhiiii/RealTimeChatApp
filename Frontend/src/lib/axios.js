47c64b21a88e29fa27fc82bcf365c07e1a7ea5f0
Samruddhi_Gulhane <90473980+Samruddhiiii@users.noreply.github.com>
2025-08-04 18:57:05 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
-import axios from "axios";
-
-
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://realtimechatapp-2-u3j7.onrender.com/api",
   withCredentials: true,
 });
